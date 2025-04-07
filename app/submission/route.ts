14ac432fdac1d9d8c3c2f282101c01f26736e881
magentera <mathew.stevenson@hotmail.co.uk>
2025-04-07 14:58:30 +1200

    convert back to post

--- app/submission/route.ts
+++ app/submission/route.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
-export async function PUT(request: Request) {
+export async function POST(request: Request) {
   const payload = await request.json(); // Parse the JSON payload
   console.log(payload); // Log the payload
   return NextResponse.json({ success: "success" });
