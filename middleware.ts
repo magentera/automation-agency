e007e78dd8692318568c32ebcffc8cc1b7081df8
magentera <mathew.stevenson@hotmail.co.uk>
2025-04-27 10:35:47 +1200

    remove middleware

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,9 @@ import type { NextRequest } from "next/server"
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+
+  return;
+  
   const isAuthenticated = request.cookies.get("authenticated")?.value === "true"
 
 
