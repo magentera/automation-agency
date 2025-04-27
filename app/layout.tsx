87e643e3a49126b5509dea4e33141f9995dc49b7
magentera <mathew.stevenson@hotmail.co.uk>
2025-04-27 20:39:20 +1200

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,34 @@ import { Inter } from "next/font/google"
 import Header from "@/components/header"
 import { cookies } from "next/headers"
 import { GoogleAnalytics } from '@next/third-parties/google'
-
+import { Metadata } from "next"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata = {
-  title: "Konuke - AI Workflows for the Enterprise",
+export const metadata: Metadata = {
+  title: 'Konuke – Workflow Optimisation & AI Automation',
   description:
-    "Augment your workforce with AI workflows. Make your organization smarter.",
-  generator: ''
-}
+    'Augment your workforce with AI workflows. Make your organization smarter.',
+  alternates: { canonical: 'https://konuke.com/' },
+  openGraph: {
+    type:  'website',
+    siteName: 'Konuke',
+    url:   'https://konuke.com/',
+    title: 'Konuke – Workflow Optimisation & AI Automation',
+    description:
+      'Lean, AI-powered workflows that slash enterprise ops costs by 38 %.',
+    images: ['https://konuke.com/og/konuke.png'],
+  },
+
+  twitter: {
+    card:        'summary_large_image',
+    site:        '@connectKonuke',
+    title:       'Augment your workforce with AI workflows. Make your organization smarter.',
+    description:
+      'LAugment your workforce with AI workflows. Make your organization smarter.',
+    images: ['https://konuke.com/og/konuke.png'],
+  },
+};
 
 export default function RootLayout({
   children,
